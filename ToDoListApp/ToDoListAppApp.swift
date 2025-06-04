3acb1b4d8d8810228652610ba64bae1b81f3dfe6
melissa <melissa>
2025-06-04 16:08:58 +0800

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Joy on 04/06/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ToDoListAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
